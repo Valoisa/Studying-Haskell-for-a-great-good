64c25e288c89c69529b827b40e95b293272d1d28
Artem Pelenitsyn <ulysses4ever@gmail.com>
2014-09-17 23:01:42 +0400

    Исправлены кавычки с ' на `
    В Хаскеле любую функцию с двумя аргументами можно записать в инфиксной
    форме, используя обратные одиночные кавычки: f a b == a `f` b
    В прямые одиночные кавычки заключаются символы.

--- MyLogic.hs
+++ MyLogic.hs
@@ -17,25 +17,25 @@ T `and3` T = T
 T `and3` U = U
 T `and3` F = F
 
-U 'and3' T = U
-U 'and3' U = U
-U 'and3' F = F
+U `and3` T = U
+U `and3` U = U
+U `and3` F = F
 
-F 'and3' T = F
-F 'and3' U = F
-F 'and3' U = F
+F `and3` T = F
+F `and3` U = F
+F `and3` U = F
 
 or3 :: Logic3 -> Logic3 -> Logic3
-T 'or3' T = T
-T 'or3' U = T
-T 'or3' F = T
+T `or3` T = T
+T `or3` U = T
+T `or3` F = T
 
-U 'or3' T = T
-U 'or3' U = U
-U 'or3' F = U
+U `or3` T = T
+U `or3` U = U
+U `or3` F = U
 
-F 'or3' T = T
-F 'or3' U = U
-F 'or3' F = F 
+F `or3` T = T
+F `or3` U = U
+F `or3` F = F 
 
 
