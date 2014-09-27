c338452640faec903e9a31bc1f7b610f04d4f9ef
Valoisa <mailbox_of_olga@mail.ru>
2014-09-27 23:14:01 +0400

    Version without pattern-matching

--- MyLogic.hs
+++ MyLogic.hs
@@ -1,39 +1,30 @@
 module MyLogic
-( Logic3 (..)
-, not3
-, and3
-, or3
-) where 	
-	data Logic3 = T|F|U	deriving (Eq, Show)
-	not3 :: Logic3 -> Logic3
-	not3 b
-		| b == T	= U
-		| b == U	= F
-		| b == F	= T		
-	
-	and3 :: Logic3 -> Logic3 -> Logic3
-	and3 x y
-		| (x == T) && (y== T)	= T
-		| (x == T) && (y== U)	= U
-		| (x == T) && (y== F)	= F
-	
-		| (x == U) && (y == T)	= U
-		| (x == U) && (y == U)	= U
-		| (x == U) && (y == F)	= F
-	
-		| (x == F) && (y == T)	= F
-		| (x == F) && (y == T)	= F
-		| (x == F) && (y == T)	= F
-	
-	or3 :: Logic3 -> Logic3 -> Logic3
-		| (x == T) || (y== T)	= T
-		| (x == T) || (y== U)	= T
-		| (x == T) || (y== F)	= T
-	
-		| (x == U) || (y == T)	= T
-		| (x == U) || (y == U)	= U
-		| (x == U) || (y == F)	= U
-	
-		| (x == F) || (y == T)	= T
-		| (x == F) || (y == T)	= U
-		| (x == F) || (y == T)	= F
+(	Logic3 (..)
+,	not3
+,	and3
+,	or3
+,	no4th
+)
+where
+
+data Logic3 = T|U|F deriving (Eq, Show)
+
+not3 :: Logic3 -> Logic3
+not3 T = U
+not3 U = F
+not3 F = T
+
+and3 :: Logic3 -> Logic3 -> Logic3
+a `and3` b
+	| (a == F) || (b == F)	= F
+	| (a == T) && (b == T)	= T
+	| otherwise				= U
+
+or3 :: Logic3 -> Logic3 -> Logic3
+a `or3` b
+	| (a == T) || (b == T)	= T
+	| (a == F) && (b == F)	= F
+	| otherwise				= U	 
+
+no4th :: Logic3 -> Logic3
+no4th a = a `and3` (not3 a) `and3` (not3 (not3 a)) 	
