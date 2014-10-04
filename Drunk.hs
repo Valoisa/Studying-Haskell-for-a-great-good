1f359bc8eb62b5c5b5ef703b4a9bdd6a6dd2592e
Valoisa <mailbox_of_olga@mail.ru>
2014-10-04 11:44:53 +0400

    Module Drunk, error in function move

---
+++ Drunk.hs
@@ -0,0 +1,33 @@
+module Drunk 
+( Suit(..)
+, Value(..)
+, Card(..)
+, sameSuit
+, beats
+, move
+{-, num_of_moves
+, read_cards_from_file
+, deal_cards -}
+)
+where
+
+data Suit = Clubs|Diamonds|Hearts|Spades deriving (Eq, Show, Read)
+data Value = Six|Seven|Eight|Nine|Ten|Jack|Queen|King|Ace deriving (Ord, Eq, Show, Read) 
+-- Как сделать джокера?
+-- Можно ли заменить слова цифрами?
+type Card = (Value, Suit)
+type Deck = [Card]
+
+sameSuit ::	Card -> Card -> Bool
+sameSuit (a, b) (x, y) = (b == y)
+
+beats :: Card -> Card -> Bool
+beats (a, b) (x, y) = (a > x)
+
+move :: (Deck, Deck) -> (Deck, Deck)
+move (x:xs, y:ys) = move_plus (x:xs, y:ys) [] -- Почему не работает?
+	where 
+	move_plus (x:xs, y:ys) zs
+	|(beats x y) == True	= (xs ++ [y] ++ [x] ++ zs, ys) 	
+	|(beats y x) == True	= (xs, ys ++ [x] ++ [y] ++ zs)
+	| otherwise				= move_plus (xs, ys) zs++[x]++[y]
