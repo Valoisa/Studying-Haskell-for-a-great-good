751e11825b8d473b89b16f442f4230db077f7f6d
Valoisa <mailbox_of_olga@mail.ru>
2014-10-04 12:23:46 +0400

    Function num_of_moves was added (commented)

--- Drunk.hs
+++ Drunk.hs
@@ -5,8 +5,8 @@ module Drunk
 , sameSuit
 , beats
 , move
-{-, num_of_moves
-, read_cards_from_file
+, num_of_moves
+{-, read_cards_from_file
 , deal_cards -}
 )
 where
@@ -18,7 +18,7 @@ data Value = Six|Seven|Eight|Nine|Ten|Jack|Queen|King|Ace deriving (Ord, Eq, Sho
 type Card = (Value, Suit)
 type Deck = [Card]
 
-sameSuit ::	Card -> Card -> Bool
+sameSuit :: Card -> Card -> Bool
 sameSuit (a, b) (x, y) = (b == y)
 
 beats :: Card -> Card -> Bool
@@ -26,8 +26,17 @@ beats (a, b) (x, y) = (a > x)
 
 move :: (Deck, Deck) -> (Deck, Deck)
 move (x:xs, y:ys) = move_plus (x:xs, y:ys) [] -- Почему не работает?
-	where 
-	move_plus (x:xs, y:ys) zs
-	|(beats x y) == True	= (xs ++ [y] ++ [x] ++ zs, ys) 	
-	|(beats y x) == True	= (xs, ys ++ [x] ++ [y] ++ zs)
-	| otherwise				= move_plus (xs, ys) zs++[x]++[y]
+    where 
+    move_plus (x:xs, y:ys) zs
+    |beats x y == True    = (xs ++ [y] ++ [x] ++ zs, ys)  
+    |beats y x == True    = (xs, ys ++ [x] ++ [y] ++ zs)
+    |otherwise            = move_plus (xs, ys) zs++[x]++[y]
+	
+num_of_moves :: (Deck, Deck) -> Int
+num_of_moves = undefined
+{- num_of_moves (xs, ys)
+	|pair == ([], ys)) || (pair == (xs, []))	= 1
+	|otherwise									= 1 + num_of_moves pair
+		where 
+		pair = move (xs, ys)
+-}	
