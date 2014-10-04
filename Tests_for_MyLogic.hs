c9ef5047ea9451a03df525305b81fb445064a217
Valoisa <mailbox_of_olga@mail.ru>
2014-10-04 12:40:01 +0400

    Test for the function from the module MyLogic

---
+++ Tests_for_MyLogic.hs
@@ -0,0 +1,29 @@
+import MyLogic
+test1 = T `and3` T
+test2 = T `and3` U
+test3 = T `and3` F
+
+test4 = U `and3` T
+test5 = U `and3` U
+test6 = U `and3` F
+
+test7 = F `and3` T
+test8 = F `and3` U
+test9 = F `and3` F
+
+test10 = T `or3` T
+test11 = T `or3` U
+test12 = T `or3` F
+
+test13 = U `or3` T
+test14 = U `or3` U
+test15 = U `or3` F
+
+test16 = F `or3` T
+test17 = F `or3` U
+test18 = F `or3` F
+
+test19 = no4th T
+test20 = no4th U
+test21 = no4th F
+
