82ee3bd25987d39bbfb884177d0933efc80b1cdd
Valoisa <mailbox_of_olga@mail.ru>
2014-11-01 10:28:57 +0300

    1.2, 1.3 work wrong

--- class-10/class-10-1.2.hs
+++ class-10/class-10-1.2.hs
@@ -3,7 +3,11 @@ totalLength :: [String] -> Int
 totalLength xs = sum $ map (length) xs
 
 main = do
-	handle <- openFile "strings.txt" ReadMode        
-	a <- fmap totalLength $ hGetContents handle
+	handle <- openFile "strings.txt" ReadMode
+	{-a <- hGetContents handle
+	lines a
+	putStrLn a -} -- Выводит содержимое правильно
+	a <- fmap (totalLength.lines) $ hGetContents handle
         hClose handle
-	putStrLn $ "Total Length of the entered Strings: " ++ show a
+	putStrLn $ "Total Length of the entered Strings: " ++ show a -- a = 0 
+	
