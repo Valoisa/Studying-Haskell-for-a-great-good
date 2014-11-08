3cbeff4b0b09cbda3e462e8c64c2c3883b2d4c74
Valoisa <mailbox_of_olga@mail.ru>
2014-11-08 14:33:09 +0300

    Type problems

--- class-10/class-10-2.1.hs
+++ class-10/class-10-2.1.hs
@@ -20,9 +20,16 @@ reduce a
 	Написать функцию, применяющую определённую выше функцию reduce 
 	заданное количество раз к значению в контексте, являющемся функтором:
 	reduceNF :: Functor f => Int -> f a -> f a
+-}
+
+--reduceNF :: Functor f => Int -> f a -> f b
+--reduceNF n f a =  last $ take n $ iterate (fmap reduce) (f a)
+	
+{-	
 	Дан текстовый файл, содержащий пары положительных целых чисел в каждой 
 	строке. Придумать способы преобразования этого списка к значению в 
 	следующих контекстах:
+	
 
     список;
     Maybe;
@@ -31,5 +38,23 @@ reduce a
 
 -}
 
---reduceNF :: Functor f => Int -> f a -> f b
---reduceNF n f a =  last $ take n $ iterate (fmap reduce) (f a)
+makePairs :: (Integral a) => [a] -> [(a, a)]
+makePairs [] = []
+makePairs (x:y:xs) = (x, y) : makePairs xs
+
+readTuples :: (Integral a) => String -> [(a, a)]
+readTuples name = do
+	conents <- readFile name
+	return $ makePairs $ map read $ words conents
+
+unpair :: (Integral a) => [(a, a)] -> [a]
+unpair xs = foldl (\acc (x, y) -> acc ++ [x] ++ [y]) [] xs
+
+pairlist2maybe :: (Integral a) => [(a, a)] -> [Maybe (a, a)]
+pairlist2maybe xs = foldl (\acc (x, y) -> if x /= 0 && y /= 0 then acc ++ [Just (x, y)] else acc ++ [Nothing]) [] xs
+
+--pairlist2either :: (Integral a) => [(a, a)] -> [Either a a]
+--pairlist2either xs = foldl (\acc (x, y) -> if x /= 0 && y /= 0 then acc ++ [Left (x, y)] else acc ++ [Right (x, y)]) [] xs
+
+pairlist2IO :: (Integral a) => [(a, a)] -> IO [(a, a)]
+pairlist2IO xs = undefined
